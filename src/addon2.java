7710ce1606300d6235ae673e38564518bac8939b
roman <roman-1901@mail.ru>
2022-11-17 22:53:19 +0300

    Homework10

--- src/addon2.java
+++ src/addon2.java
@@ -5,7 +5,6 @@ public class addon2 {
     public static void main(String[] args) {
         String fullName = "димонов димон димонович";
         char[] fullNameArray = fullName.toCharArray();
-        StringBuffer result = new StringBuffer();
         for (int i = 0; i < fullNameArray.length; i++) {
             if (i == 0) {
                 fullNameArray[i] = fullName.toUpperCase().charAt(i);
@@ -13,9 +12,8 @@ public class addon2 {
             if (fullNameArray[i] == ' ') {
                 fullNameArray[i + 1] = fullName.toUpperCase().charAt(i + 1);
             }
-            result.append(fullNameArray[i]);
         }
-        fullName = result.toString();
+        fullName = String.valueOf(fullNameArray);
         System.out.print(fullName);
     }
 }
